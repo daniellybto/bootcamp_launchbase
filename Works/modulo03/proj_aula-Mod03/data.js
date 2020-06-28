f0ebaeabe8c3a53b8e3c867e6140e6b283b94c3e
Dany.Win10 <danielly.eng@hotmail.com>
2020-06-28 10:10:42 -0300

    Backend quase finalizada - utilizando Template Engine Nunjucks!

---
+++ Works/modulo03/proj_aula-Mod03/data.js
@@ -0,0 +1,59 @@
+module.exports = [
+  {
+    id: "i6Oi-YtXnAU",
+    title: "Curso Javascript Completo 2020 [Iniciantes] + 14 Mini-Projetos | Dev Aprender",
+    duration: "4:35 horas",
+    price: "FREE"
+  },
+  {
+    id: "7U3-pJZkN-o",
+    title: "8 Projetos em Python para iniciantes [Código+GUI] | Dev Aprender",
+    duration: "2:31 horas",
+    price: "FREE"
+  },
+  {
+    id: "6wn8hpUcEcM",
+    title: "Drag and Drop Raiz com a API do HTML, CSS e JavaScript | Masterclass #16",
+    duration: "55:38 min",
+    price: "FREE",
+    featured: true
+  },
+  {
+    id: "x-4z_u8LcGc",
+    title: "CSS Grid Layout e Flexbox - Quando Utilizar | Origamid",
+    duration: "39:08 min",
+    price: "FREE"
+  },
+  {
+    id: "HN1UjzRSdBk",
+    title: "Unveiling CSS Grid in practice | Mayk Brito",
+    duration: "36:16 min",
+    price: "FREE"
+  },
+  {
+    id: "DiXbJL3iWVs",
+    title: "Node.js: Iniciando da teoria à prática | Masterclass #11 | Mayk Brito",
+    duration: "01:33 horas",
+    price: "FREE"
+  },
+  {
+    id: "BvhYm0BOLvA",
+    title: "Dark Mode raiz com CSS e JavaScript | Code/Drops #24 | Mayk Brito",
+    duration: "17:34 min",
+    price: "FREE",
+    featured: true
+  },
+  {
+    id: "ngVU74daJ8Y",
+    title: "Tema light/dark com React, Styled Components e TypeScript | Code/Drops #16 | Rocketseat",
+    duration: "33:31 min",
+    price: "FREE", 
+    featured: true
+  },
+  {
+    id: "Fbu7z5dXcRs",
+    title: "SQL no Node.js com Sequelize | Masterclass #01 | Rocketseat",
+    duration: "1:40 horas",
+    price: "FREE"
+  }
+]
---
+++ Works/modulo03/proj_aula-Mod03/server.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+
+const server = express()
+const videos = require("./data")
+
+server.use(express.static('public'))
+
+server.set("view engine", "njk")
+
+nunjucks.configure("views", {
+  express: server,
+  autoescape: false, //permite passar URLs por texto normal
+  noCache: true
+})
+
+
+// ---- ROTAS ---- //
+server.get("/", function(req, res){
+  const about = {
+    avatar_url: "me.jpeg",
+    name: "Danielly Brito",
+    role: "Estudante Full Stack - Bootcamp LauchBase 2.0",
+    description: "Programadora Full Stack Jr - Estudante - Estudando sempre as mais novas tecnologias utilizadas no mercado!",
+    links: [{
+      name: "Github",
+      url: "https://github.com/daniellybto"
+    },
+    {
+      name: "Instagram",
+      url: "https://www.instagram.com/daniellybto/"
+    },
+    {
+      name: "LinkedIn",
+      url: "https://www.linkedin.com/in/daniellybto"
+    }]
+  }
+  
+  return res.render("about", {about: about})
+})
+server.get("/portfolio", function(req, res){
+  return res.render("portfolio" , {items: videos} )
+})
+
+
+
+// ---- CONFIGURAR PORTA DO SERVIDOR! ---- //
+server.listen(5000, function(){
+  console.log("Server is running!!!!")
+})
